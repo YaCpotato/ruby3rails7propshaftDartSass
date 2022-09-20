5ab03e91cab1c4bc2c704e1dd0abdf825e1c5eb9
YaCpotato <yasshisshy@gmail.com>
2022-09-21 03:15:25 +0900

    Sortable.js OK

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.9/dist/flatpickr.js"
+pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js"
